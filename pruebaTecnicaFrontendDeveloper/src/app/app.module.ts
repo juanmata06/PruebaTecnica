b91c1622ca4d8ab7f4b9e44e28eba2d1e97d42f7
Juan Mata <mataguzzojuan@gmail.com>
2022-06-16 21:46:45 +0200

    Creacion de Proyecto

---
+++ pruebaTecnicaFrontendDeveloper/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SideBarComponent } from './modules/side-bar/side-bar.component';
+import { ProfileCardComponent } from './templates/profile-card/profile-card.component';
+import { ProfileScreenComponent } from './modules/profile-screen/profile-screen.component';
+import { ProfileViewComponent } from './templates/profile-view/profile-view.component';
+import { ContactsViewComponent } from './templates/contacts-view/contacts-view.component';
+import { ContactsListComponent } from './templates/contacts-list/contacts-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideBarComponent,
+    ProfileCardComponent,
+    ProfileScreenComponent,
+    ProfileViewComponent,
+    ContactsViewComponent,
+    ContactsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ pruebaTecnicaFrontendDeveloper/src/app/templates/contacts-view/contacts-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-view',
+  templateUrl: './contacts-view.component.html',
+  styleUrls: ['./contacts-view.component.css']
+})
+export class ContactsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
